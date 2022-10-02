4784f2f7d5e89b0c48187a448a3ca2e66a9e73fc
DeathAvenger00 <asoham60@gmail.com>
2022-10-02 21:20:24 +0530

    Initial Commit

---
+++ Netfix clone/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netfix clone
+//
+//  Created by Soham.C.Athawale on 02/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
